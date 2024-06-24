2b0486d188a571149b3e4fe7fbe340b108eb9a19
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-06-24 22:41:05 +0800

    remove unnecessary lines

--- routes/sales.js
+++ routes/sales.js
@@ -1,28 +1,23 @@
 // routes/sales.js
 
 const express = require('express');
-const pool = require('../db'); // Import your database pool
-
+const pool = require('../db');
 const router = express.Router();
 
-// Define the endpoint for fetching all sales data with total sum
 router.get('/', async (req, res) => {
   try {
-    // Concurrently fetch sales data and total sum of 'total' column
-    const [sales, totalSum] = await Promise.all([
+    const [sales, totalSum, totalSumToday] = await Promise.all([
       getSalesFromDatabase(),
-      getSumOfTotalSales()
+      getSumOfTotalSales(),
+      getSumOfTotalSalesToday()
     ]);
 
-    // Construct the response object
     const responseData = {
       sales: sales,
-      total_sum: totalSum
+      total_sum: totalSum,
+      total_sum_today: totalSumToday
     };
 
-    console.log('sales', responseData)
-
-    // Send JSON response
     res.json(responseData);
   } catch (error) {
     console.error('Error fetching sales and total sum from database:', error);
@@ -30,7 +25,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Function to fetch sales data from the database
 async function getSalesFromDatabase() {
   const queryText = 'SELECT * FROM sales ORDER BY id DESC';
   const { rows } = await pool.query(queryText);
@@ -38,21 +32,18 @@ async function getSalesFromDatabase() {
   return rows;
 }
 
-// Function to fetch total sum of 'total' column from 'sales' table
 async function getSumOfTotalSales() {
   const queryText = 'SELECT SUM(total) AS total_sum FROM sales';
   const { rows } = await pool.query(queryText);
-  return rows[0].total_sum; // Extract the total sum from the first row
+  return rows[0].total_sum;
 }
 
-// Function to fetch total sum of 'total' column from 'sales' table
 async function getSumOfTotalSalesToday() {
   const queryText = 'SELECT SUM(total) AS total_sum_today FROM sales WHERE DATE(datetime) = DATE(NOW())';
   const { rows } = await pool.query(queryText);
-  return rows[0].total_sum_today; // Extract the total sum from the first row
+  return rows[0].total_sum_today;
 }
 
-// Define the endpoint for fetching total sum of sales
 router.get('/total-sum', async (req, res) => {
   try {
     const totalSum = await getSumOfTotalSales();
@@ -63,7 +54,6 @@ router.get('/total-sum', async (req, res) => {
   }
 });
 
-// Define the endpoint for fetching total sum of sales
 router.get('/total-sum-today', async (req, res) => {
   try {
     const totalSumToday = await getSumOfTotalSalesToday();
