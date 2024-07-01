af9496ee8dd5c97066bd1f8d9c7219f448764a51
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-01 11:13:10 +0800

    upload

--- routes/expenses.js
+++ routes/expenses.js
@@ -150,13 +150,24 @@ module.exports = function (wss) {
         if (!req.file) {
             return res.status(400).send('No file uploaded.');
         }
-        const file = req.file;
-        console.log({file});
-        const blob = put(file.originalname, file, { access: 'public' , token : process.env.BLOB_READ_WRITE_TOKEN,  });
-        console.log({blob});
-        res.json({blob});
-    });
+        try {
+            const file = req.file;
+            console.log({ file });
     
+            // Upload file to Vercel Blob
+            const blob = put(file.originalname, file, {
+                access: 'public',
+                token: process.env.BLOB_READ_WRITE_TOKEN,
+            });
+            console.log({ blob });
+    
+            // Respond with blob details
+            res.json({ blob });
+        } catch (error) {
+            console.error('Error uploading file to Vercel Blob:', error);
+            res.status(500).json({ error: 'Failed to upload file to Vercel Blob' });
+        }
+    });
 
     router.put('/:id/pay', async (req, res) => {
         try {
