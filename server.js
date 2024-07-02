4d2bbb4e3a7249719bb87588fd4741963bc8a19d
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-02 13:37:22 +0800

    expenses

--- server.js
+++ server.js
@@ -285,11 +285,11 @@ function addExpenses(newExpenses) {
               console.log('New expense added:', newExpense);
               resolve(newExpense);
 
-              wss.clients.forEach(client => {
-                  if (client.readyState === WebSocket.OPEN) {
-                      client.send(JSON.stringify({ action: 'addExpensesResponse', expense: newExpense }));
-                  }
-              });
+              // wss.clients.forEach(client => {
+              //     if (client.readyState === WebSocket.OPEN) {
+              //         client.send(JSON.stringify({ action: 'addExpensesResponse', expense: newExpense }));
+              //     }
+              // });
           }
       );
   });
