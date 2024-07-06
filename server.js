d511ea381ec6fe2aba30e0fdaf19f9640318c26d
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-06 22:01:46 +0800

    expenses

--- server.js
+++ server.js
@@ -272,7 +272,7 @@ function addExpenses(newExpenses) {
   return new Promise((resolve, reject) => {
       const { expense, month, date, amount, mode_of_payment, image_path, credit, paid_by, settled_by } = newExpenses;
       pool.query(
-          'INSERT INTO expenses (expense, month, date, amount, mode_of_payment, image_path, credit, paid_by, settled_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
+          'INSERT INTO expenses (expense, month, date, amount, mode_of_payment, image_path, credit, paid_by, settled_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
           [expense, month, date, amount, mode_of_payment, image_path, credit, paid_by, settled_by],
           (error, results) => {
               if (error) {
