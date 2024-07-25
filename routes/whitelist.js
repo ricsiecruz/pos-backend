db7b412c0913afd87346388af8350adfc0f7cdeb
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-07-26 01:08:01 +0800

    whitelist

--- routes/whitelist.js
+++ routes/whitelist.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const pool = require('../db'); // Adjust the path according to your setup
-const app = express();
 const router = express.Router();
 
 // Configure Express to trust the first proxy if behind one
-app.set('trust proxy', true);
+router.use((req, res, next) => {
+    req.app.set('trust proxy', true);
+    next();
+});
 
 const normalizeIp = (ip) => {
     if (ip === '::1') {
@@ -16,7 +18,12 @@ const normalizeIp = (ip) => {
     return ip;
 };
 
-router.use(async (req, res, next) => {
+router.get('/ip', (req, res) => {
+    const clientIp = normalizeIp(req.ip);
+    res.send(`Your IP is ${clientIp}`);
+});
+
+router.get('/', async (req, res) => {
     try {
         const clientIp = normalizeIp(req.ip); // Normalize the IP address
         console.log('Client IP:', clientIp);
@@ -27,8 +34,7 @@ router.use(async (req, res, next) => {
 
         if (rows.length > 0) {
             console.log('You have access');
-            res.json({message: 'success'})
-            // next(); // IP is whitelisted and enabled, proceed to the next middleware/route handler
+            res.json({ message: 'success' }); // IP is whitelisted and enabled
         } else {
             console.log('You shall not pass');
             res.status(403).json({ error: 'Access denied' }); // IP is not whitelisted or not enabled
@@ -37,11 +43,6 @@ router.use(async (req, res, next) => {
         console.error('Error checking IP whitelist:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-});
-
-router.get('/ip', (req, res) => {
-    const clientIp = normalizeIp(req.ip);
-    res.send(`Your IP is ${clientIp}`);
 });
 
 module.exports = router;
