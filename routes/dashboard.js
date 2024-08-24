538a5496868d1bf1eeb102530823d08509406de0
Ricsie Cruz <ricsie_aguila@yahoo.com>
2024-08-24 22:33:19 +0800

    limit to 10

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,10 +3,12 @@ const pool = require('../db');
 const moment = require('moment-timezone');
 const router = express.Router();
 
+const TIMEZONE = 'Asia/Manila';
+
 router.get('/', async (req, res) => {
     try {
         const [mostOrderedToday, mostOrdered, salesAndExpensesSummary, topSpenders, updatedMembers, startDate] = await Promise.all([
-            getMostOrderedToday(),
+            getMostOrdered(true),
             getMostOrdered(),
             getSalesAndExpensesSummary(),
             getTopSpenders(),
@@ -14,125 +16,106 @@ router.get('/', async (req, res) => {
             getStartDate()
         ]);
 
-        const responseData = {
-            mostOrderedToday: mostOrderedToday,
-            mostOrdered: mostOrdered,
-            salesAndExpensesSummary: salesAndExpensesSummary,
-            topSpenders: topSpenders,
-            updatedMembers: updatedMembers,
-            startDate: startDate
-        };
-
-        res.json(responseData);
+        res.json({
+            mostOrderedToday,
+            mostOrdered,
+            salesAndExpensesSummary,
+            topSpenders,
+            updatedMembers,
+            startDate
+        });
     } catch (error) {
-        console.error('Error fetching dashboard from database:', error);
-        res.status(500).json({ error: 'Internal server error - dashboard' });
+        handleError(res, 'Error fetching dashboard from database:', error, 'Internal server error - dashboard');
     }
 });
 
-async function getMostOrderedToday() {
-    const queryText = `
+async function getMostOrdered(isToday = false) {
+    let queryText = `
         WITH expanded_orders AS (
             SELECT 
-                jsonb_array_elements(sales.orders::jsonb) AS order_detail
-            FROM sales
-            WHERE DATE(sales.datetime AT TIME ZONE 'Asia/Manila') = CURRENT_DATE
-        )
-        SELECT 
-            order_detail ->> 'product' AS product,
-            SUM((order_detail ->> 'quantity')::integer) AS total_quantity
-        FROM expanded_orders
-        GROUP BY order_detail ->> 'product'
-        ORDER BY total_quantity DESC;
+                o.order_detail
+            FROM 
+                sales,
+                LATERAL (
+                    SELECT 
+                        jsonb_array_elements(orders::jsonb) AS order_detail
+                    WHERE 
+                        jsonb_typeof(orders::jsonb) = 'array'
+                ) o
     `;
-    const { rows } = await pool.query(queryText);
-    return rows;
-}
+    
+    if (isToday) {
+        queryText += ` WHERE DATE(datetime AT TIME ZONE '${TIMEZONE}') = CURRENT_DATE `;
+    }
 
-async function getMostOrdered() {
-    const queryText = `
-        WITH expanded_orders AS (
-            SELECT 
-                jsonb_array_elements(orders::jsonb) AS order_detail
-            FROM sales
+    queryText += `
         )
         SELECT 
             order_detail ->> 'product' AS product,
             SUM((order_detail ->> 'quantity')::integer) AS total_quantity
         FROM expanded_orders
         GROUP BY order_detail ->> 'product'
-        ORDER BY total_quantity DESC;
+        ORDER BY total_quantity DESC
+        LIMIT 10;
     `;
+
     const { rows } = await pool.query(queryText);
     return rows;
 }
 
 async function getSalesAndExpensesSummary() {
     const queryText = `
-    WITH sales_summary AS (
+        WITH sales_summary AS (
+            SELECT 
+                DATE(datetime AT TIME ZONE '${TIMEZONE}') AS date,
+                SUM(total::numeric) AS total_sales
+            FROM 
+                sales
+            GROUP BY 
+                DATE(datetime AT TIME ZONE '${TIMEZONE}')
+        ),
+        expenses_summary AS (
+            SELECT 
+                DATE(date AT TIME ZONE '${TIMEZONE}') AS date,
+                SUM(amount::numeric) AS total_expenses
+            FROM 
+                expenses
+            GROUP BY 
+                DATE(date AT TIME ZONE '${TIMEZONE}')
+        ),
+        summary AS (
+            SELECT 
+                COALESCE(sales_summary.date, expenses_summary.date) AS date,
+                COALESCE(sales_summary.total_sales, 0) AS total_sales,
+                COALESCE(expenses_summary.total_expenses, 0) AS total_expenses,
+                COALESCE(sales_summary.total_sales, 0) - COALESCE(expenses_summary.total_expenses, 0) AS net
+            FROM 
+                sales_summary
+            FULL OUTER JOIN 
+                expenses_summary 
+            ON 
+                sales_summary.date = expenses_summary.date
+        )
         SELECT 
-            DATE(datetime AT TIME ZONE 'Asia/Manila') AS date,
-            SUM(total::numeric) AS total_sales
+            date,
+            total_sales,
+            total_expenses,
+            net
         FROM 
-            sales
-        GROUP BY 
-            DATE(datetime AT TIME ZONE 'Asia/Manila')
-    ),
-    expenses_summary AS (
-        SELECT 
-            DATE(date AT TIME ZONE 'Asia/Manila') AS date,
-            SUM(amount::numeric) AS total_expenses
-        FROM 
-            expenses
-        GROUP BY 
-            DATE(date AT TIME ZONE 'Asia/Manila')
-    ),
-    summary AS (
-        SELECT 
-            COALESCE(sales_summary.date, expenses_summary.date) AS date,
-            COALESCE(sales_summary.total_sales, 0) AS total_sales,
-            COALESCE(expenses_summary.total_expenses, 0) AS total_expenses,
-            COALESCE(sales_summary.total_sales, 0) - COALESCE(expenses_summary.total_expenses, 0) AS net
-        FROM 
-            sales_summary
-        FULL OUTER JOIN 
-            expenses_summary 
-        ON 
-            sales_summary.date = expenses_summary.date
-    )
-    SELECT 
-        date,
-        total_sales,
-        total_expenses,
-        net
-    FROM 
-        summary
-    WHERE 
-        total_sales > 0
-    ORDER BY 
-        total_sales DESC;
+            summary
+        WHERE 
+            total_sales > 0
+        ORDER BY 
+            total_sales DESC;
     `;
 
     const { rows } = await pool.query(queryText);
 
-    if (rows.length === 0) {
-        return {
-            data: [],
-            all_time_low: null,
-            all_time_high: null
-        };
-    }
-
-    const all_time_low = rows[rows.length - 1];
-    const all_time_high = rows[0];
-
-    const summary = {
+    return {
         data: rows,
-        all_time_low,
-        all_time_high
+        all_time_low: rows[rows.length - 1],
+        all_time_high: rows[0]
     };
-
-    return summary;
 }
 
 async function getTopSpenders(today = false) {
@@ -147,14 +130,15 @@ async function getTopSpenders(today = false) {
     `;
 
     if (today) {
-        queryText += ` WHERE DATE(datetime AT TIME ZONE 'Asia/Manila') = CURRENT_DATE `;
+        queryText += ` WHERE DATE(datetime AT TIME ZONE '${TIMEZONE}') = CURRENT_DATE `;
     }
 
     queryText += `
         GROUP BY 
             customer
         ORDER BY 
-            total_spent DESC;
+            total_spent DESC
+        LIMIT 10;
     `;
 
     const { rows } = await pool.query(queryText);
@@ -178,7 +162,7 @@ async function updateMemberData() {
         WITH member_sales AS (
             SELECT 
                 customer,
-                MAX(datetime AT TIME ZONE 'Asia/Manila') AS last_spent,
+                MAX(datetime AT TIME ZONE '${TIMEZONE}') AS last_spent,
                 SUM(computer::numeric) AS total_computer,
                 SUM(subtotal::numeric) AS total_subtotal
             FROM 
@@ -195,25 +179,10 @@ async function updateMemberData() {
         FROM 
             member_sales ms
         WHERE 
-            members.name = ms.customer;
+            members.name = ms.customer
+        RETURNING id, name, total_load, coffee, total_spent, members.last_spent AT TIME ZONE '${TIMEZONE}' AS last_spent;
     `;
-    await pool.query(updateQueryText);
-
-    // Fetch the updated member data
-    const fetchUpdatedMembersQueryText = `
-        SELECT 
-            id, 
-            name, 
-            total_load, 
-            coffee, 
-            total_spent,
-            last_spent AT TIME ZONE 'Asia/Manila' AS last_spent
-        FROM 
-            members
-        ORDER BY 
-            total_spent DESC;
-    `;
-    const { rows } = await pool.query(fetchUpdatedMembersQueryText);
+    const { rows } = await pool.query(updateQueryText);
 
     const formatter = new Intl.NumberFormat('en-US', {
         minimumFractionDigits: 2,
@@ -224,22 +193,10 @@ async function updateMemberData() {
         row.total_load = formatter.format(row.total_load);
         row.coffee = formatter.format(row.coffee);
         row.total_spent = formatter.format(row.total_spent);
-        row.last_spent = moment(row.last_spent).tz('Asia/Manila').format('YYYY-MM-DD HH:mm:ss');
+        row.last_spent = moment(row.last_spent).tz(TIMEZONE).format('YYYY-MM-DD HH:mm:ss');
     });
 
     return rows;
-}
-
-async function getStartDate() {
-    const queryText = `
-        SELECT 
-            MIN(datetime AT TIME ZONE 'Asia/Manila') AS start_date
-        FROM 
-            sales;
-    `;
-    const { rows } = await pool.query(queryText);
-    const startDate = rows[0].start_date;
-    return moment(startDate).tz('Asia/Manila').format('YYYY-MM-DD HH:mm:ss');
 }
 
 router.get('/most-ordered', async (req, res) => {
@@ -281,5 +238,21 @@ router.get('/top-spenders-today', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+async function getStartDate() {
+    const queryText = `
+        SELECT 
+            MIN(datetime AT TIME ZONE '${TIMEZONE}') AS start_date
+        FROM 
+            sales;
+    `;
+    const { rows } = await pool.query(queryText);
+    return rows[0].start_date;
+}
+
+function handleError(res, logMessage, error, clientMessage) {
+    console.error(logMessage, error);
+    res.status(500).json({ message: clientMessage });
+}
 
 module.exports = router;
